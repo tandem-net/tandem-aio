862b0d25c4f0ee73546d3eedd0522e76a3a62655
penguin <penguin586970@gmail.com>
2026-06-06 16:20:25 -0400

    fix vibe slop

--- tandem-client/src/serialization.rs
+++ tandem-client/src/serialization.rs
@@ -1,42 +1,10 @@
 // serialization.rs
 // handles serialization and deserialization of Python objects
 
-// TODO: Implement cloudpickle or alternative serialization
-// Options:
-// - cloudpickle-rs (if available)
-// - Custom binary protocol
-// - MessagePack for cross-language compatibility
-// - Protocol Buffers
-
-/// Deserializes a Python object from bytes
-/// 
-/// Currently supports cloudpickle format
-/// Can be extended to support other formats
-pub fn deserialize(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    // TODO: Implement actual deserialization
-    // For now, return the data as-is
-    println!("Deserializing {} bytes", data.len());
-    
+pub fn deserialize(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> {
     Ok(data.to_vec())
 }
 
-/// Serializes a Python object to bytes
-/// 
-/// Currently supports cloudpickle format
-/// Can be extended to support other formats
-pub fn serialize(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    // TODO: Implement actual serialization
-    // For now, return the data as-is
-    println!("Serializing {} bytes", data.len());
-    
+pub fn serialize(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> {
     Ok(data.to_vec())
-}
-
-/// Validates if data is valid cloudpickle format
-#[allow(dead_code)]
-fn is_valid_cloudpickle(data: &[u8]) -> bool {
-    // TODO: Implement validation logic
-    // Check for cloudpickle magic bytes/headers
-    
-    !data.is_empty()
 }
