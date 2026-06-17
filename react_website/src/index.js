c3b84a2651a6c8a7b97ab7d0ca28394e6cc0fd79
Squash882 <ambatipudivishal3@gmail.com>
2026-06-16 20:00:08 -0400

    aryan color pallete suggestion

--- react_website/src/index.js
+++ react_website/src/index.js
@@ -10,20 +10,20 @@ export default class App extends Component {
     return ( // add all website content inside scrollview so it's not just a static page.
 /*
  _____               _                
-|_   _|_ _ _ __   __| | ___ _ __ ___  
-  | |/ _` | '_ \ / _` |/ _ \ '_ ` _ \ 
-  | | (_| | | | | (_| |  __/ | | | | |
-  |_|\__,_|_| |_|\__,_|\___|_| |_| |_|
+|_   _|_ _ _ __   __| | ___ _ __ ___   
+  | |/ _` | '_ \ / _` |/ _ \ '_ ` _ \  
+  | | (_| | | | | (_| |  __/ | | | | |  
+  |_|\__,_|_| |_|\__,_|\___|_| |_| |_| 
 */
 
       <View contentContainerStyle={styles.container}>
         <View style={styles.topBar}>
           {/* add top bar content here, such as logo and navigation links */}
         </View>
-      <ScrollView>
-        <View style={styles.card}>
-          <Text style={styles.paragraph}>Welcome to Tandem!</Text>
-        </View>
+        <ScrollView>
+          <View style={styles.card}>
+            <Text style={styles.paragraph}>Welcome to Tandem!</Text>
+          </View>
         </ScrollView>
       </View>
     );
@@ -44,12 +44,12 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
-    color: '#34495e',
+    color: '#4d2e00',
   },
   topBar: {
     justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#65d6d4',
+    backgroundColor: '#dba400',
     width: deviceWidth,
     height: deviceHeight/7
   }
