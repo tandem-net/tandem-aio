d7220d666c04f13168d6b11331ec5b7f483eac93
wnusair <sam.nusair@gmail.com>
2026-06-18 23:18:11 -0400

    bugfix, linked upload and download to main.rs

--- node/src/tasks.rs
+++ node/src/tasks.rs
@@ -1,6 +1,6 @@
 use futures::future::BoxFuture;
 use reqwest::{Client, Error};
-use create::measure;
+use crate::measure;
 
 pub fn download_task(client: Client, url: impl Into<String>) -> BoxFuture<'static, Result<(), Error>> {
     let url = url.into();
@@ -17,7 +17,7 @@ pub fn upload_task(client: Client, url: impl Into<String>, bytes: usize) -> BoxF
     let url = url.into();
 
     Box::pin(async move {
-        let duration = measure::measure_upload(client, url, bytes).await?;
+        let duration = measure::measure_upload(client, &url, bytes, 65536).await?;
         let mb = bytes as f64 / (1024.0 * 1024.0);
 
         println!("uploaded {:.2} MB in {:.2}s ({:.2} MB/s", mb, duration, mb / duration);
