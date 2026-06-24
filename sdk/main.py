2db9747010c521bb6131ec46e65ceff1c1353adc
wnusair <sam.nusair@gmail.com>
2026-06-24 18:20:49 -0400

    Placeholder wissam-ize command in cli
    Co-authored-by: MrPlankton611 <shamgardavid0@gmail.com>

---
+++ sdk/main.py
@@ -0,0 +1,13 @@
+# Wissam Respect LOC
+
+
+def test():
+    print("helllo")
+    print(1 + 2)
+    
+    x = 6 # This is a variable called 'x' that sets the number 6 equal to 'x'
+    y = 7
+
+    z = x + y
+
+    print(f"{x} + {y} test + {z}")
